ab75f8a81ef0ab3a232b98aeaffd8da2d1981c1d
cliffchew84 <cliffchew84@gmail.com>
2023-08-01 11:36:16 +0800

    added search result status code 400 error capture

--- main.py
+++ main.py
@@ -506,8 +506,8 @@ async def show_search_books(request: Request,
 
     if book_search:
         books = nlb_rest_api.get_rest_nlb_api("SearchTitles/", book_search)
-        if books.get("totalRecords") == 0:
-            text_output = f"There are no records with the phase {book_search}"
+        if books.get("totalRecords") == 0 or books.get("statusCode") == 400:
+            text_output = f"There are no records with '{book_search}'"
             book_search = None
 
         else:
