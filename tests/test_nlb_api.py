963941476e715bf31e589ee45dda293ddc8d00cd
cliffchew84 <cliffchew84@gmail.com>
2023-08-28 23:13:27 +0800

    Set up first proper unit test

--- tests/test_nlb_api.py
+++ tests/test_nlb_api.py
@@ -1,12 +1,13 @@
 import os
-from nlb_rest_api import get_rest_nlb_api
+from sg_nlb_avail_bks.nlb_rest_api import get_rest_nlb_api
 
 APPLICATION_ID = os.environ['nlb_rest_app_id']
 API_KEY = os.environ['nlb_rest_api_key']
 
 
 def test_api_call():
-    output = get_rest_nlb_api(extension_url="SearchTiles",
+    output = get_rest_nlb_api(extension_url="SearchTitles",
                               input="Python",
                               search_on="Title")
-    assert (output.get('setId') == 502893088)
+
+    assert (output.get('setId') == 503031189)
