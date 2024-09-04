c95ff737a478a408d4f109316abcc7bc3e33840a
cliffchew84 <cliffchew84@gmail.com>
2024-09-04 10:16:54 +0800

    Adding unit test code

---
+++ conftest.py
@@ -0,0 +1,5 @@
+import sys
+import os
+
+# Add the directory where conftest.py is located to the sys.path
+sys.path.append(os.path.dirname(__file__))
---
+++ test_code.py
@@ -0,0 +1,11 @@
+import os
+from nlb_rest_api import get_bk_data 
+
+APPLICATION_ID = os.environ['nlb_rest_app_id']
+API_KEY = os.environ['nlb_rest_api_key']
+
+
+def test_api_call():
+    print('Simple test get_bk_data API call')
+    output = get_bk_data(ext_url="SearchTitles", input="Python")
+    assert (output.get('hasMoreRecords') is True)
