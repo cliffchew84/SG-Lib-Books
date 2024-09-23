04027d1402266fe0cc9d6c3b092a453ead76c241
cliffchew84 <cliffchew84@gmail.com>
2024-09-23 19:00:22 +0800

    Update NLB API pull to get more data from GetTitleDetails

--- nlb_api.py
+++ nlb_api.py
@@ -4,12 +4,15 @@ import requests
 import pendulum
 from typing import Dict, List
 
-# This script focuses on functions that interacts with the NLB API. I have added
-# a bit more robust code by checking if the code hits any statusCode 429 issue,
-# which is the rate limiting error from NLB. I also tried to make sure that all
-# dictionary convrsions from NLB uses .get("var_name", None), to ensure
-# that I at least always have that parameter as a None if it is missing.
-# Hopefully this helps with reducing any error at the ingestion into MongoDB.
+# This script focuses on functions that interacts with the NLB API. I added
+# more robust code to check if code hits statusCode 429, which is the rate 
+# limiting error from NLB.
+#
+# I added .get("var_name", None) to my dicts to make them more robust
+#
+# Lastly, I refactored my get_bk_data to include GetTitles, to make my NLB API
+# calls more consistent. I am also trying to see if I can use GetTitles rather
+# than GetTitleDetails, but GetTitles do not use BRN
 
 # Add os.environ for authentication
 APPLICATION_ID = os.environ['nlb_rest_app_id']
@@ -21,48 +24,70 @@ def auth_nlb(app_id, api_key):
 
 
 def get_bk_data(ext_url: str,
-                input: str,
+                input_dict: Dict,
                 offset=None,
                 app_id: str = APPLICATION_ID,
                 api_key: str = API_KEY) -> Dict:
-    """ Using one function to call all REST API methods
-        ext_url param: (1) SearchTitles (2) GetAvailabilityInfo (3) GetTitleDetails
-        search_on param: Keyword Search
+    """ Get all NLB REST API books data
+        ext_url: GetAvailabilityInfo 
+                 GetTitleDetails ( to delete )
+                 GetTitles
+        input_dict: {"Author": input}
+                    {"Title": input}
+                    {"BRN": input}
+    [TODO]
+    - GetTitleDetails provide less info then GetTitles. However, GetTitles can't
+      use BRN, which makes sense cos GetTitles does a general search, while 
+      using BRN is a very specific search. Closest usage is ISBN, but that
+      isn't unique like BRN.
+    - For now, consider just adding details from GetTitleDetails first 
     """
+
     headers = auth_nlb(app_id, api_key)
     final_url = "https://openweb.nlb.gov.sg/api/v2/Catalogue/" + ext_url
-    payload = {'BRN': input, "Limit": 100}
-    if ext_url == "SearchTitles":
-        payload = {'Keywords': input}
-        if offset:
-            payload.update({"Offset": offset})
-    
-    json_output = requests.get(final_url, headers=headers, params=payload).json() 
+    # [TODO] - Should I consider just make all limit == 30 ? 
+    if ext_url == "GetAvailabilityInfo" or ext_url == "GetTitleDetails":
+        input_dict.update({"limit": 100})
+
+    elif ext_url == "GetTitles":
+        # GetTitles don't accept BRN in input_dict
+        input_dict.update({"limit": 30})
+
+    if offset:
+        input_dict.update({"Offset": offset})
+
+    json_output = requests.get(
+        final_url, headers=headers, params=input_dict).json() 
 
     while json_output.get('statusCode') == 429:
         print("Please wait for awhile. We are hitting NLB too hard!")
         time.sleep(2)
-        json_output = requests.get(final_url, headers=headers, params=payload).json() 
-    
+        json_output = requests.get(
+            final_url, headers=headers, params=input_dict).json() 
+
     return json_output
 
+
 def process_bk_info(nlb_input: Dict) -> Dict:
-    """ Process book info output from NLB rest API """
+    """ Process book info output from NLB API - GetTitleDetails"""
+
+    subjects = list(set(nlb_input.get('subjects', [])))
 
     return {
         'BID' : nlb_input.get('brn', None),
         'TitleName': nlb_input.get('title', None),
-        'Author': nlb_input.get('author', None), 
-        'PublishYear' : nlb_input.get('publishDate', None)
+        'Author': nlb_input.get('author', None),
+        'PublishYear' : nlb_input.get('publishDate', None),
+        'Subjects' : subjects,
+        'Publisher' : nlb_input.get('publisher', None),
     }
 
 
-def get_process_bk_info(db, bid_no):
+def get_process_bk_info(bid_no):
     """ Make API calls to book info and ingest into DB """
-    raw_bk_title = get_bk_data("GetTitleDetails", input=bid_no)
+    raw_bk_title = get_bk_data("GetTitleDetails", input_dict={"BRN": bid_no})
     bk_title = process_bk_info(raw_bk_title) 
     bk_title.update({"BID": str(bid_no)})
-    del bk_title['PublishYear']
 
     title = bk_title.get("TitleName", None)
     if title:
@@ -70,35 +95,12 @@ def get_process_bk_info(db, bid_no):
     return bk_title
 
 
-def bk_search(input_dict: Dict,
-              offset=None,
-              app_id: str = APPLICATION_ID,
-              api_key: str = API_KEY) -> Dict:
-    """ Get GetTitles using APIs"""
-    headers = auth_nlb(app_id, api_key)
-    final_url = "https://openweb.nlb.gov.sg/api/v2/Catalogue/GetTitles"
-    input_dict.update({"limit": 30})
-    if offset:
-        input_dict.update({"Offset": offset})
-
-    json_output = requests.get(
-        final_url, headers=headers, params=input_dict).json()
-
-    while json_output.get('statusCode') == 429:
-        print("Please wait for awhile. We are hitting NLB too hard!")
-        time.sleep(2)
-        json_output = requests.get(
-            final_url, headers=headers, params=input_dict).json()
-
-    return json_output
-
-
 def check_bk_avail(bid_no: int):
     """ To check the availability of book by count 
         if bk.get('count') == 0, this means that this
         book isn't searchable on the actual NLB app!
     """
-    bk = get_bk_data("GetAvailabilityInfo", input=bid_no)
+    bk = get_bk_data("GetAvailabilityInfo", input_dict={"BRN": bid_no})
     return bk.get("count")
 
 
