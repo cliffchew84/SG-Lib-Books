5a442288849545759d087e967791721a935a4cb1
kiritowu <wongzhaowu@gmail.com>
2024-10-21 22:15:48 +0800

    [feat] Migrate nav and library endpoints

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,11 +2,15 @@ from fastapi import APIRouter
 
 from src.api.auth import router as auth_router
 from src.api.books import router as books_router
+from src.api.library import router as library_router
+from src.api.nav import router as nav_router
 from src.api.search import router as search_router
 from src.api.user import router as user_router
 
 api = APIRouter()
 api.include_router(auth_router, prefix="/auth", tags=["auth"])
 api.include_router(books_router, prefix="/books", tags=["books"])
+api.include_router(library_router, prefix="/lib", tags=["lib"])
+api.include_router(nav_router, prefix="/nav", tags=["nav"])
 api.include_router(search_router, prefix="/search", tags=["search"])
 api.include_router(user_router, prefix="/user", tags=["user"])
--- src/api/nav.py
+++ src/api/nav.py
@@ -1,83 +1,58 @@
-from fastapi import (
-    FastAPI,
-    status,
-    Request,
-    Depends,
-    Cookie,
-)
-from fastapi.responses import RedirectResponse, HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
+from fastapi import APIRouter, Request, status
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 
-from src import m_db
+from src.api.deps import SDBDep, MDBDep, UsernameDep
 from src import supa_db as s_db
+from src import m_db
 from src import process as p
-from src.api import api
-from src.api.deps import SDBDep, UsernameDep
-from src.config import settings
 from src.utils import templates
 
 
-# Think about adding back mongoDB stuff
-# user_status
-
-# Environment setup
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+router = APIRouter()
 
 
-# Application code
-app = FastAPI(
-    title=settings.APP_NAME, version=settings.VERSION, description=settings.DESCRIPTION
-)
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app.include_router(api)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/main", response_class=HTMLResponse)
-async def main(request: Request, username: UsernameDep, db: SDBDep):
+@router.get("", response_class=HTMLResponse)
+async def update_header(
+    request: Request, db: SDBDep, mdb: MDBDep, username: UsernameDep
+):
     if not username:
         return RedirectResponse("/", status_code=status.HTTP_302_FOUND)
 
-    # Continue to extract user book info
-    query = s_db.q_user_bks(username)
+    query = s_db.q_user_bks(username=username)
     response = p.process_user_bks(query)
+
     # Processing necessary statistics
     all_unique_books = p.get_unique_bks(response)
     all_avail_books = p.get_avail_bks(response)
     unique_libs = p.get_unique_libs(response)
     avail_bks_by_lib = p.get_avail_bks_by_lib(response)
     lib_book_summary = p.get_lib_bk_summary(unique_libs, avail_bks_by_lib)
-    mdb = m_db.connect_mdb()
-    mdb = mdb["nlb"]
+
     update_status = None
-    if m_db.q_status(db=mdb, username=username):
+    if m_db.q_status(db=mdb.nlb, username=username):
         update_status = " "
+
     # Check if user has a default library
     user_info = s_db.q_user_info(db, username)
     preferred_lib = user_info.get("preferred_lib")
+
     if preferred_lib:
         preferred_lib = preferred_lib.lower()
         output = []
         for book in response:
             if preferred_lib in book["BranchName"].lower():
                 output.append(book)
+
     else:
         preferred_lib = "all"
         output = response
+
     lib_avail = len(p.get_avail_bks(output))
     lib_all = len(p.get_unique_bks(output))
+
     return templates.TemplateResponse(
-        "main.html",
+        "navbar.html",
         {
             "request": request,
             "username": username,
@@ -92,12 +67,3 @@ async def main(request: Request, username: UsernameDep, db: SDBDep):
             "status": update_status,
         },
     )
-
-
-# Main Page
-@app.get("/", response_class=HTMLResponse)
-async def root(request: Request, username: UsernameDep):
-    if username:
-        # Redirect to main if user is logged in
-        return RedirectResponse("/main")
-    return templates.TemplateResponse("google_page.html", {"request": request})
