b62f03e05f35f803629c742e4a1d32eca2e0a65f
kiritowu <wongzhaowu@gmail.com>
2024-11-27 17:49:40 +0800

    [fix] Code linting

--- front-end/src/routes/dashboard/+layout.svelte
+++ front-end/src/routes/dashboard/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import type { LayoutData } from './$types';
+
 	import Header from '$lib/components/layout/Header.svelte';
 	import Footer from '$lib/components/layout/Footer.svelte';
+
 	let { children, data }: { children: Snippet; data: LayoutData } = $props();
 </script>
 
