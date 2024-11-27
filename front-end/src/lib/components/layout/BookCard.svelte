0d9274bc88137e4c29bfa1e70c87e6e7bb347a7c
kiritowu <wongzhaowu@gmail.com>
2024-11-27 23:58:08 +0800

    [wip] Complete UI for search page

---
+++ front-end/src/lib/components/layout/BookCard.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import { type MouseEventHandler } from 'svelte/elements';
+	import {
+		ExternalLink,
+		Calendar,
+		User,
+		AlignJustify,
+		MapPin,
+		BookMarked,
+		Bookmark
+	} from 'lucide-svelte';
+
+	// TODO: migrate book modals into interface file
+	interface PageProps {
+		title: string;
+		brn: number;
+		author: string;
+		publishYear: string;
+		callNumber: string;
+		branches: string[];
+		imageLink: string;
+		bookmarked: boolean;
+		onBookMarked: MouseEventHandler<HTMLButtonElement>;
+	}
+
+	let {
+		brn,
+		title = '',
+		author = '',
+		publishYear = '',
+		callNumber = '',
+		branches = [],
+		imageLink = '',
+		bookmarked = false,
+		onBookMarked = () => {}
+	}: PageProps = $props();
+
+	let externalLink = $derived(`https://catalogue.nlb.gov.sg/search/card?recordId=${brn}`);
+	let branchesName = $derived(branches.join(', '));
+</script>
+
+<div class="rounded-lg shadow relative border-slate-400">
+	<a class="bg-muted min-h-[170px]" href={`/dashboard/books/${brn}`}>
+		{#if imageLink}
+			<img src={imageLink} alt={title} class="rounded-t-lg" />
+		{/if}
+	</a>
+
+	<button onclick={onBookMarked} class="absolute right-3 top-3 z-50 shadow">
+		{#if bookmarked}
+			<BookMarked class="w-5 h-5 bg-muted" />
+		{:else}
+			<Bookmark class="w-5 h-5 bg-muted" />
+		{/if}
+	</button>
+	<div class="my-4 mx-3 flex flex-col gap-2 text-xs">
+		<div class="flex justify-between">
+			<h3
+				class="text-ellipsis overflow-hidden whitespace-nowrap text-slate-700 font-semibold text-sm"
+			>
+				{title}
+			</h3>
+			<a href={externalLink} target="_blank">
+				<ExternalLink class="w-4 h-4" />
+			</a>
+		</div>
+		{#if author}
+			<div class="flex gap-2 text-slate-500">
+				<User class="w-4 h-4 my-auto" />
+				<p class="text-ellipsis overflow-hidden whitespace-nowrap">{author}</p>
+			</div>
+		{/if}
+		{#if publishYear}
+			<div class="flex gap-2 text-slate-500">
+				<Calendar class="w-4 h-4 my-auto " />
+				<p class="text-ellipsis overflow-hidden whitespace-nowrap">{publishYear}</p>
+			</div>
+		{/if}
+		{#if callNumber}
+			<div class="flex gap-2 text-slate-500">
+				<AlignJustify class="w-4 h-4 my-auto" />
+				<p class="text-ellipsis overflow-hidden whitespace-nowrap">{callNumber}</p>
+			</div>
+		{/if}
+		{#if branchesName}
+			<div class="flex gap-2 text-slate-500">
+				<MapPin class="w-6 h-4 my-auto" />
+				<p class="text-ellipsis overflow-hidden whitespace-nowrap">{branchesName}</p>
+			</div>
+		{/if}
+	</div>
+</div>
---
+++ front-end/src/lib/components/layout/PaginatedCards.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import * as Pagination from '$lib/components/ui/pagination';
+	import BookCard from './BookCard.svelte';
+
+	let { books = [], perPage = 25 }: { books: { [key: string]: any }[]; perPage: number } = $props();
+	let page = $state(0); // Current page number
+	let count = $derived(books.length); // Total items
+	let filteredBooks = $derived(books.slice(page * perPage, Math.min((page + 1) * perPage, count))); // Slice of books based on page changes
+</script>
+
+<section class="flex flex-col gap-3">
+	<div class="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-3">
+		{#each filteredBooks as bookEle, id}
+			<BookCard brn={id} {...bookEle} />
+		{/each}
+	</div>
+	<Pagination.Root {count} {perPage} bind:page let:pages let:currentPage>
+		<Pagination.Content>
+			<Pagination.Item>
+				<Pagination.PrevButton />
+			</Pagination.Item>
+			{#each pages as page (page.key)}
+				{#if page.type === 'ellipsis'}
+					<Pagination.Item>
+						<Pagination.Ellipsis />
+					</Pagination.Item>
+				{:else}
+					<Pagination.Item isVisible={currentPage == page.value}>
+						<Pagination.Link {page} isActive={currentPage == page.value}>
+							{page.value}
+						</Pagination.Link>
+					</Pagination.Item>
+				{/if}
+			{/each}
+			<Pagination.Item>
+				<Pagination.NextButton />
+			</Pagination.Item>
+		</Pagination.Content>
+	</Pagination.Root>
+</section>
