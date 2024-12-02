8449ad726fe2a86df5430f3d0a75d5583b0c4a8d
kiritowu <wongzhaowu@gmail.com>
2024-12-02 20:17:20 +0800

    [fix] Ensure frontend is whitelisted and run init_super_client before fastapi server is initiated

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import (
     FastAPI,
 )
@@ -5,12 +7,26 @@ from fastapi.middleware.cors import CORSMiddleware
 
 
 from src.api.main import api_router
+from src.api.deps import init_super_client
 from src.config import settings
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    """Lifespan for FastAPI Server"""
+    # Initialise supabase connection
+    await init_super_client()
+
+    # Yield server to receive requests
+    yield
 
 
 # Application code
 app = FastAPI(
-    title=settings.APP_NAME, version=settings.VERSION, description=settings.DESCRIPTION
+    title=settings.APP_NAME,
+    version=settings.VERSION,
+    description=settings.DESCRIPTION,
+    lifespan=lifespan,
 )
 
 if settings.BACKEND_CORS_ORIGINS:
