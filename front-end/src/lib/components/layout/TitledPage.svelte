15714afe76342fd3b7ca01e16025194171c69fe3
kiritowu <wongzhaowu@gmail.com>
2024-12-02 17:21:41 +0800

    [fix] Add min-screen-height to pages

--- front-end/src/lib/components/layout/TitledPage.svelte
+++ front-end/src/lib/components/layout/TitledPage.svelte
@@ -8,7 +8,7 @@
 	}: { children: Snippet; title: String; description: String } = $props();
 </script>
 
-<main class="container flex flex-col gap-8 p-8">
+<main class="container flex flex-col gap-8 p-8 min-h-[85vh]">
 	<div class="flex flex-col gap-3">
 		<h1 class="text-4xl font-bold text-slate-700">{title}</h1>
 		<p class="text-sm text-slate-500">{description}</p>
