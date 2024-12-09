7f3eb3d1c48395a3d65763e1a00c14466dd01cbe
kiritowu <wongzhaowu@gmail.com>
2024-12-09 21:43:17 +0800

    [fix] Add 127.0.0.1 to backend cors whitelist

--- back-end/src/config.py
+++ back-end/src/config.py
@@ -16,6 +16,7 @@ class Settings(BaseSettings):
         "https://127.0.0.1:8000",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://127.0.0.1:4173",
         "http://localhost:5173",
         "localhost",
         "https://sg-nlb-available-books.onrender.com",
