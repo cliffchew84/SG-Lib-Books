0e5eaffda53eba32ef79bd52425310ffe2b842bc
kiritowu <wongzhaowu@gmail.com>
2024-12-15 13:24:36 +0800

    [wip] Add few methods for book avail CRUD operation by BID

--- back-end/src/crud/book_avail.py
+++ back-end/src/crud/book_avail.py
@@ -27,6 +27,10 @@ class CRUDBookAvail(CRUDBase[BookAvail, BookAvailCreate, BookAvailUpdate]):
     async def get_all(self, db: Client) -> list[BookAvail]:
         return await super().get_all(db)
 
+    async def get_all_by_bid(self, db: Client, bid: int) -> list[BookAvail]:
+        response = db.table(self.model.table_name).select("*").eq("BID", bid).execute()
+        return [self.model(**item) for item in response.data]
+
     async def get_multi_by_owner(
         self, db: Client, *, username: str, BIDs: Optional[list[int]] = None
     ) -> list[BookAvail]:
@@ -54,11 +58,33 @@ class CRUDBookAvail(CRUDBase[BookAvail, BookAvailCreate, BookAvailUpdate]):
         *,
         obj_ins: list[BookAvailCreate],
         excludes: Optional[set[str]] = {"UpdateTime"},
+        delete_excluded: bool = True,
     ) -> list[BookAvail]:
-        return await super().upsert(db, obj_ins=obj_ins, excludes=excludes)
+        updated_book_avail = await super().upsert(
+            db, obj_ins=obj_ins, excludes=excludes
+        )
+        updated_book_avail_itemNo = {updated.ItemNo for updated in updated_book_avail}
+        if updated_book_avail and delete_excluded:
+            old_book_avail = await self.get_all_by_bid(
+                db, bid=updated_book_avail[0].BID
+            )
+            old_book_avail_itemNo = {book.ItemNo for book in old_book_avail}
+            for remove_ItemNo in old_book_avail_itemNo.difference(
+                updated_book_avail_itemNo
+            ):
+                await self.delete(db, i=remove_ItemNo)
+
+        return updated_book_avail
 
     async def delete(self, db: Client, *, i: str) -> BookAvail | None:
         return await super().delete(db, i=i)
+
+    async def delete_by_bid(self, db: Client, *, bid: int) -> BookAvail | None:
+        response = db.table(self.model.table_name).delete().eq("BID", bid).execute()
+        deleted = response.data
+        if not deleted:
+            return None
+        return self.model(**deleted[0])
 
 
 book_avail_crud = CRUDBookAvail(BookAvail)
