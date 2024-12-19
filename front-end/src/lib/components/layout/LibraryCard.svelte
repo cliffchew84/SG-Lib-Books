fb736a23e0f9f090d66f5093e0dcc9ba9add007a
kiritowu <wongzhaowu@gmail.com>
2024-12-20 00:11:44 +0800

    [fix] Add empty sstate when library availibity is empty

--- front-end/src/lib/components/layout/LibraryCard.svelte
+++ front-end/src/lib/components/layout/LibraryCard.svelte
@@ -16,9 +16,11 @@
 	let noOnLoan = $derived(onLoanBooks.length);
 
 	let availabilityStatus = $derived(
-		[noAvail ? `${noAvail} Available` : undefined, noOnLoan ? `${noOnLoan} On-Loan` : undefined]
-			.filter((v) => v)
-			.join(' · ')
+		noAvail > 0 || noOnLoan > 0
+			? [noAvail ? `${noAvail} Available` : undefined, noOnLoan ? `${noOnLoan} On-Loan` : undefined]
+					.filter((v) => v)
+					.join(' · ')
+			: 'No related books'
 	);
 </script>
 
