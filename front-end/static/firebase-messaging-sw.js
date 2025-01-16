c10b92e76abe61d938fc76f122828842b686b9e1
kiritowu <wongzhaowu@gmail.com>
2025-01-16 18:01:27 +0800

    [feat] Add firebase service worker to receive notification onbackground

---
+++ front-end/static/firebase-messaging-sw.js
@@ -0,0 +1,42 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+// Replace 10.13.2 with latest version of the Firebase JS SDK.
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+	apiKey: "AIzaSyDOTjTDM6PojM84ZMUeDoGrByPD7PjPjow",
+	authDomain: "sg-lib-books.firebaseapp.com",
+	projectId: "sg-lib-books",
+	storageBucket: "sg-lib-books.firebasestorage.app",
+	messagingSenderId: "955327746756",
+	appId: "1:955327746756:web:222c40ec81a9e83488af91",
+	measurementId: "G-VEBNMX6GCV"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+	console.log(
+		'[firebase-messaging-sw.js] Received background message ',
+		payload
+	);
+	const notificationTitle = payload.notification.title;  // Or `payload.notification` depending on what the payload is
+	var notificationOptions = {
+		body: payload.notification.body,
+		icon: payload.data.icon,
+		data: { url: payload.data.click_action },  // The URL which we are going to use later
+		actions: [{ action: "open_url", title: "Check Out" }],
+	};
+
+	return self.registration.showNotification(
+		notificationTitle,
+		notificationOptions,
+	);
+});
