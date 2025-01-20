ee30fc8de146074e6a66049742e67bc536c5e5eb
kiritowu <wongzhaowu@gmail.com>
2025-01-20 21:24:36 +0800

    [feat] Add db endpoint for notification manipulation

---
+++ back-end/src/api/routes/notifications.py
@@ -0,0 +1,97 @@
+from fastapi import APIRouter, status, HTTPException
+
+from src.api.deps import SDBDep, CurrentUser
+from src.crud.notifications import notification_crud
+from src.modals.notifications import Notification, NotificationUpdate
+
+router = APIRouter()
+
+
+@router.get("")
+async def get_notifications(
+    user: CurrentUser,
+    db: SDBDep,
+) -> list[Notification]:
+    """Get all libraries in the database and whether is library favourite"""
+    if not user or not user.email:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Email is not found for user")
+
+    try:
+        return await notification_crud.get_multi_by_owner(db, username=user.email)
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            "Error occured with database transaction.",
+        ) from e
+
+
+@router.post("/{nid}/read")
+async def read_notification(
+    nid: str,
+    db: SDBDep,
+    user: CurrentUser,
+) -> None:
+    if not user or not user.email:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Email is not found for user")
+
+    try:
+        notification = await notification_crud.get(db, i=nid)
+        if not notification:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND,
+                f"Notification of {nid} could not be found from database.",
+            )
+        if notification.UserName != user.email:
+            raise HTTPException(
+                status.HTTP_401_UNAUTHORIZED,
+                "User is not authorized to read this notification.",
+            )
+
+        await notification_crud.update(
+            db,
+            obj_in=NotificationUpdate(isRead=True),
+            i=nid,
+        )
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            "Error occured with database transaction.",
+        ) from e
+
+
+# Unread notifications
+@router.post("/{nid}/unread")
+async def unread_notification(
+    nid: str,
+    db: SDBDep,
+    user: CurrentUser,
+) -> None:
+    if not user or not user.email:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Email is not found for user")
+
+    try:
+        notification = await notification_crud.get(db, i=nid)
+        if not notification:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND,
+                f"Notification of {nid} could not be found from database.",
+            )
+        if notification.UserName != user.email:
+            raise HTTPException(
+                status.HTTP_401_UNAUTHORIZED,
+                "User is not authorized to read this notification.",
+            )
+
+        await notification_crud.update(
+            db,
+            obj_in=NotificationUpdate(**notification.model_dump(), isRead=False),
+            i=nid,
+        )
+    except Exception as e:
+        raise HTTPException(
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            "Error occured with database transaction.",
+        ) from e
